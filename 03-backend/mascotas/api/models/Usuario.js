dd291316b3375abc00f91f92d8a596b837f71a6c
Paul Reinoso <washington.reinoso@epn.edu.ec>
2020-08-17 08:58:07 -0500

    Relaciones y asociaciones con sails

--- 03-backend/mascotas/api/models/Usuario.js
+++ 03-backend/mascotas/api/models/Usuario.js
@@ -4,26 +4,34 @@
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
-
 module.exports = {
-
+  tableName:'epn_usuario',
   attributes: {
-
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    cedula:{ //nombre del atributo
+      type:'String',
+      required:true,
+      columnName: 'epn_cedula',
+      unique:true,
+      minLength:10,
+      maxLength:25,
+    },
+    nombre:{
+      type:'String',
+      required:true,            
+      minLength:3
+    },
+    correo:{
+      type:'String',
+      isEmail:true
+    },
+    estdoCivil:{
+      type:'String',
+      isIn:['Soltero','Casado','Divorciado','Viudo','Union Libre'],
+      defaultsTo:'Soltero'
+    },
+    password:{
+      type:'String',
+      regex:/^[a-zA-Z]\w{3,14}$/
+    }
   },
-
 };
-
