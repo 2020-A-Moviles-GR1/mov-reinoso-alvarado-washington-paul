618d881b88cfe6f290cc7518d64cdbb2ccfd86ac
Paul Reinoso <washington.reinoso@epn.edu.ec>
2020-09-24 23:33:53 -0500

    imagen en marcador y url habierta en el navegador

---
+++ Examen1BUnivAlien/app/src/main/java/com/example/examen_1b_univ_alien/PicassoMarker.kt
@@ -0,0 +1,32 @@
+package com.example.examen_1b_univ_alien
+
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
+import com.squareup.picasso.Picasso
+
+//class PicassoMarker { }
+
+class PicassoMarker internal constructor(var mMarker: Marker) : com.squareup.picasso.Target {
+    override fun hashCode(): Int {
+        return mMarker.hashCode()
+    }
+
+    override fun equals(o: Any?): Boolean {
+        return if (o is PicassoMarker) {
+            val marker = o.mMarker
+            mMarker == marker
+        } else {
+            false
+        }
+    }
+
+    override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+        mMarker.setIcon(BitmapDescriptorFactory.fromBitmap(bitmap))
+    }
+
+    override fun onBitmapFailed(errorDrawable: Drawable?) {}
+    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
+
+}
